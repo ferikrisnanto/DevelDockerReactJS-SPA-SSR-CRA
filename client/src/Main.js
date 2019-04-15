0dba952fee619c890faec99c1bf05720d3a60aca
Feri Krisnanto <ferrykrisnanto312@gmail.com>
2019-04-15 12:01:31 +0700

    apa aja deh

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,6 +3,7 @@ import { Route, NavLink, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import TestApi from "./components/TestApi";
  
 class Main extends Component {
   render() {
@@ -14,10 +15,12 @@ class Main extends Component {
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink to="/test-api">Test API</NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
+            <Route path="/test-api" component={TestApi}/>
             <Route path="/contact" component={Contact}/>
           </div>
         </div>
